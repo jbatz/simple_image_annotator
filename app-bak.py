f2269cbf917186d48a8947468064c92d8c42ca20
Joseph Batz <joseph.e.batz@intel.com>
2018-06-05 14:13:01 -0700

    moving tagger to grader

---
+++ app-bak.py
@@ -0,0 +1,121 @@
+import sys
+from os import walk
+import imghdr
+import csv
+import argparse
+
+from flask import Flask, redirect, url_for, request
+from flask import render_template
+from flask import send_file
+
+
+app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+@app.route('/tagger')
+def tagger():
+    if (app.config["HEAD"] == len(app.config["FILES"])):
+        return redirect(url_for('bye'))
+    directory = app.config['IMAGES']
+    image = app.config["FILES"][app.config["HEAD"]]
+    labels = app.config["LABELS"]
+    not_end = not(app.config["HEAD"] == len(app.config["FILES"]) - 1)
+    print(not_end)
+    return render_template('grader.html', not_end=not_end, directory=directory, image=image, labels=labels, head=app.config["HEAD"] + 1, len=len(app.config["FILES"]))
+
+@app.route('/next')
+def next():
+    image = app.config["FILES"][app.config["HEAD"]]
+    app.config["HEAD"] = app.config["HEAD"] + 1
+    with open(app.config["OUT"],'a') as f:
+        for label in app.config["LABELS"]:
+            f.write(image + "," +
+            label["id"] + "," +
+            label["name"] + "," +
+            str(round(float(label["xMin"]))) + "," +
+            str(round(float(label["xMax"]))) + "," +
+            str(round(float(label["yMin"]))) + "," +
+            str(round(float(label["yMax"]))) + "\n")
+    app.config["LABELS"] = []
+    return redirect(url_for('tagger'))
+
+@app.route("/bye")
+def bye():
+    return send_file("taf.gif", mimetype='image/gif')
+
+@app.route('/add/<id>')
+def add(id):
+    xMin = request.args.get("xMin")
+    xMax = request.args.get("xMax")
+    yMin = request.args.get("yMin")
+    yMax = request.args.get("yMax")
+    app.config["LABELS"].append({"id":id, "name":"", "xMin":xMin, "xMax":xMax, "yMin":yMin, "yMax":yMax})
+    return redirect(url_for('tagger'))
+
+@app.route('/remove/<id>')
+def remove(id):
+    index = int(id) - 1
+    del app.config["LABELS"][index]
+    for label in app.config["LABELS"][index:]:
+        label["id"] = str(int(label["id"]) - 1)
+    return redirect(url_for('tagger'))
+
+@app.route('/label/<id>')
+def label(id):
+    name = request.args.get("name")
+    app.config["LABELS"][int(id) - 1]["name"] = name
+    return redirect(url_for('tagger'))
+
+# @app.route('/prev')
+# def prev():
+#     app.config["HEAD"] = app.config["HEAD"] - 1
+#     return redirect(url_for('tagger'))
+
+@app.route('/image/<f>')
+def images(f):
+    images = app.config['IMAGES']
+    return send_file(images + f)
+
+
+
+def main():
+    arg_parser = argparse.ArgumentParser(description="extractor")
+    arg_parser.add_argument('--image_dir', dest='image_dir', required=True)
+    arg_parser.add_argument('--label_dir', dest='label_dir', required=True)
+    arg_parser.add_argument('--csv', dest='csv', required=True)
+    args = arg_parser.parse_args()
+
+    convert_files(args.in_dir, args.out_dir)
+
+
+if __name__ == "__main__":
+    main()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('image_dir', type=str, help='specify the images directory')
+    parser.add_argument('mask_dir', type=str, help='specify the mask directory')
+    parser.add_argument("--csv")
+    args = parser.parse_args()
+
+    app.config["IMAGE_DIR"] = args.image_dir
+    app.config["MASK_DIR"] = args.mask_dir
+    app.config["LABELS"] = []
+    files = None
+    for (dir_path, dir_names, mask_files) in walk(app.config["MASK_DIR"]):
+        mask_files
+        break
+    if files == None:
+        print("No files")
+        exit()
+    app.config["MASK_FILES"] = files
+    app.config["HEAD"] = 0
+    if args.csv:
+        app.config["OUT"] = "out.csv"
+    else:
+        app.config["OUT"] = args.out
+    print(files)
+    with open("out.csv",'w') as f:
+        f.write("image,id,name,xMin,xMax,yMin,yMax\n")
+    app.run(debug="True")
